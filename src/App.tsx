369eb198c0453f4c3bd4893486ee425713f6e4e0
sir-hector <krauskarol7@gmail.com>
2022-11-01 21:17:30 +0100

    add routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,20 @@
-import { useState } from 'react'
+import { useState } from "react";
+import {Routes, Route} from 'react-router-dom'
+import {Container} from 'react-bootstrap'
+import Home from "./pages/Home";
+import Store from "./pages/Store";
+import About from "./pages/About";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <div className="App"> 
-    </div>
-      
+  <Container>
+    <Routes>
+      <Route path='' element={<Home/>}/>
+      <Route path='/store' element={<Store/>}/>
+      <Route path='/about' element={<About/>}/>
+    </Routes>
+  </Container>
   )
 }
 
-export default App
+export default App;
