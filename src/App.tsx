1674af00ce84c616542a79dd18215ab90012d0ea
sir-hector <krauskarol7@gmail.com>
2022-11-01 21:41:50 +0100

    create navbar component

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,24 @@
 import { useState } from "react";
-import {Routes, Route} from 'react-router-dom'
-import {Container} from 'react-bootstrap'
+import { Routes, Route } from "react-router-dom";
+import { Container } from "react-bootstrap";
 import Home from "./pages/Home";
 import Store from "./pages/Store";
 import About from "./pages/About";
+import Navbar from "./components/Navbar";
 
 function App() {
   return (
-  <Container>
-    <Routes>
-      <Route path='' element={<Home/>}/>
-      <Route path='/store' element={<Store/>}/>
-      <Route path='/about' element={<About/>}/>
-    </Routes>
-  </Container>
-  )
+    <>
+      <Navbar />
+      <Container>
+        <Routes>
+          <Route path="" element={<Home />} />
+          <Route path="/store" element={<Store />} />
+          <Route path="/about" element={<About />} />
+        </Routes>
+      </Container>
+    </>
+  );
 }
 
 export default App;
